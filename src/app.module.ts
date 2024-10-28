37813cde9242d0d921b9ff2eaa2d230f18f2cd37
Elbaraa Radwan <77853@must.edu.eg>
2024-10-28 20:21:07 +0300

    Creating the Sturcture of the Project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { OrganizationModule } from './organization/organization.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    UserModule,
+    PrismaModule,
+    OrganizationModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
 })
 export class AppModule {}
