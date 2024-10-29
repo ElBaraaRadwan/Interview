eb8f22347f8c08f2b3db738d1976829b5d11634e
Elbaraa Radwan <77853@must.edu.eg>
2024-10-29 20:01:59 +0300

    Creating E2E Structure for Testing the APP

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,9 +2,13 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as pactum from 'pactum';
 import { AppModule } from './../src/app.module';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateAuthDto } from 'src/auth/dto';
+import { CreateOrganizationDto } from 'src/organization/dto';
 
 describe('APP E2E', () => {
   let app: INestApplication;
+  let Prisma: PrismaService;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -19,13 +23,44 @@ describe('APP E2E', () => {
     );
     await app.init();
     await app.listen(3333);
+
+    //Clean Db before each test
+    Prisma = app.get(PrismaService);
+    await Prisma.cleanDB(); // This is a custom method to clean the database
+    pactum.request.setBaseUrl('http://localhost:3333'); // This is a custom method to set the base url for the tests
   });
 
   afterAll(async () => {
     await app.close();
   });
 
-  describe('Auth', () => {});
+  describe('Auth', () => {
+    const dto: CreateAuthDto = {
+      name: 'Test User',
+      email: 'test@test.com',
+      password: 'password',
+    };
 
-  describe('Organization', () => {});
+    describe('Signup', () => {});
+
+    describe('Signin', () => {});
+  });
+
+  describe('Organization', () => {
+    const dto: CreateOrganizationDto = {
+      name: 'Test Organization',
+      description: 'Test Description',
+    };
+    describe('Create', () => {});
+
+    describe('Invite', () => {});
+
+    describe('findAll', () => {});
+
+    describe('findOne', () => {});
+
+    describe('Update', () => {});
+
+    describe('Remove', () => {});
+  });
 });
