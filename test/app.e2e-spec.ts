032188fd22ed8a78d95066bcf700ba6e7ad75854
Elbaraa Radwan <77853@must.edu.eg>
2024-10-29 19:38:47 +0300

    Start working on E2E Tests useing pactum

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,9 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import * as pactum from 'pactum';
 import { AppModule } from './../src/app.module';
 
-describe('AppController (e2e)', () => {
+describe('APP E2E', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
@@ -12,13 +12,20 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+      }),
+    );
     await app.init();
+    await app.listen(3333);
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(async () => {
+    await app.close();
   });
+
+  describe('Auth', () => {});
+
+  describe('Organization', () => {});
 });
